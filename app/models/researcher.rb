20dd35c50b86ed2b5f1249404040655b50da6461
duhaime <douglas.duhaime@gmail.com>
2016-01-31 18:28:57 -0500

    initialize all researchers to group_id 1 presently

--- app/models/researcher.rb
+++ app/models/researcher.rb
@@ -2,8 +2,8 @@ class Researcher < ActiveRecord::Base
 	belongs_to :search_group
 	before_save :set_search_group_id
 
- # Set the search group id for the current researcher
+  # Set the search group id for the current researcher
   def set_search_group_id
-    self.search_group_id = 5
+    self.search_group_id = 1
   end
 end
