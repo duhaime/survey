516f2fba2547d2d833d8ce6714030a6ece7dcf43
duhaime <douglas.duhaime@gmail.com>
2016-01-31 08:27:42 -0500

    before saving a new researcher, set their search group id to 5

--- app/models/researcher.rb
+++ app/models/researcher.rb
@@ -1,3 +1,9 @@
 class Researcher < ActiveRecord::Base
 	belongs_to :search_group
+	before_save :set_search_group_id
+
+ # Set the search group id for the current researcher
+  def set_search_group_id
+    self.search_group_id = 5
+  end
 end
