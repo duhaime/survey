c3d18e24905a90018c1046cc659ad04bdb5a2905
duhaime <douglas.duhaime@gmail.com>
2016-02-03 08:40:19 -0500

    change @search_id and @researcher_id fields to integer fields in migration file

--- db/migrate/20160131230903_create_researcher_rankings.rb
+++ db/migrate/20160131230903_create_researcher_rankings.rb
@@ -1,8 +1,8 @@
 class CreateResearcherRankings < ActiveRecord::Migration
   def change
     create_table :researcher_rankings do |t|
-      t.boolean :search_id
-      t.boolean :researcher_id
+      t.integer :search_id
+      t.integer :researcher_id
       t.boolean :result_1
       t.boolean :result_2
       t.boolean :result_3
