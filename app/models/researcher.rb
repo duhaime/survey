64660fae545af501e8c35b55039863b41f39770e
duhaime <douglas.duhaime@gmail.com>
2016-02-04 00:08:54 -0500

    add validation to ensure researcher creation form is complete

--- app/models/researcher.rb
+++ app/models/researcher.rb
@@ -1,6 +1,7 @@
 class Researcher < ActiveRecord::Base
 	belongs_to :search_group
 	before_save :set_search_group_id
+  validates :email, :name, :university, :position, presence: true
 
   # Set the search group id for the current researcher
   def set_search_group_id
