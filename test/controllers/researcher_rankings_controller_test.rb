0fb74158c0322d503b79b5d5c4086f85fd220f29
duhaime <douglas.duhaime@gmail.com>
2016-02-14 15:29:08 -0500

    removed legacy tests and updated params for remaining tests

--- test/controllers/researcher_rankings_controller_test.rb
+++ test/controllers/researcher_rankings_controller_test.rb
@@ -14,7 +14,7 @@ class ResearcherRankingsControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
-    get :new
+    get :new, search_id: @search.search_id, researcher_id: @researcher.id
     assert_response :success
   end
 
@@ -23,29 +23,15 @@ class ResearcherRankingsControllerTest < ActionController::TestCase
       post :create, researcher_ranking: { researcher_id: @researcher_ranking.researcher_id, result_8: @researcher_ranking.result_8, result_5: @researcher_ranking.result_5, result_4: @researcher_ranking.result_4, result_9: @researcher_ranking.result_9, result_1: @researcher_ranking.result_1, result_7: @researcher_ranking.result_7, result_6: @researcher_ranking.result_6, result_10: @researcher_ranking.result_10, result_3: @researcher_ranking.result_3, result_2: @researcher_ranking.result_2, search_id: @researcher_ranking.search_id }
     end
 
-    assert_redirected_to researcher_ranking_path(assigns(:researcher_ranking))
+    # the default test here assumed we would always direct users
+    # to show the created user, but we intend to either prompt
+    # the user with a question or a survey completed page
+    #assert_redirected_to researcher_ranking_path(assigns(:researcher_ranking))
   end
 
   test "should show researcher_ranking" do
-    get :show, id: @researcher_ranking
+    get :show, id: @researcher_ranking.researcher_id
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @researcher_ranking
-    assert_response :success
-  end
-
-  test "should update researcher_ranking" do
-    patch :update, id: @researcher_ranking, researcher_ranking: { researcher_id: @researcher_ranking.researcher_id, result_8: @researcher_ranking.result_8, result_5: @researcher_ranking.result_5, result_4: @researcher_ranking.result_4, result_9: @researcher_ranking.result_9, result_1: @researcher_ranking.result_1, result_7: @researcher_ranking.result_7, result_6: @researcher_ranking.result_6, result_10: @researcher_ranking.result_10, result_3: @researcher_ranking.result_3, result_2: @researcher_ranking.result_2, search_id: @researcher_ranking.search_id }
-    assert_redirected_to researcher_ranking_path(assigns(:researcher_ranking))
-  end
-
-  test "should destroy researcher_ranking" do
-    assert_difference('ResearcherRanking.count', -1) do
-      delete :destroy, id: @researcher_ranking
-    end
-
-    assert_redirected_to researcher_rankings_path
-  end
 end
