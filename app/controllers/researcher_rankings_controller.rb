85fa0259d2870f4bf583a27711b44c2f93c7441f
duhaime <douglas.duhaime@gmail.com>
2016-03-12 16:08:56 -0500

    complete rewrite of researcher rankings controller to expose only researcher email and search number in params

--- app/controllers/researcher_rankings_controller.rb
+++ app/controllers/researcher_rankings_controller.rb
@@ -12,33 +12,65 @@ class ResearcherRankingsController < ApplicationController
   def show
   end
 
+
   # GET /researcher_rankings/new
   def new
     @researcher_ranking = ResearcherRanking.new
 
-    # Set instance variables for search, researcher_id, and search_results
-    @search_id = params[:search_id]
-    @researcher_id = params[:researcher_id]
-    @search_results = SearchResult.where(search_id: @search_id)
-    @search_phrase = retrieve_search_phrase(@search_id)
+    # Set instance variables for the researcher and their current question number
+    @researcher_email = params[:researcher_email] 
+    @search_number = params[:search_number]
+
+    # retrieve information on the search we want the researcher to evaluate
+    @current_search_metadata = find_current_search_metadata(@researcher_email, @search_number)
+
+    # retrieve the search for this search
+    @current_search = find_current_search(@current_search_metadata)
+
+    # pass the search phrase to the client for display
+    @search_phrase = @current_search.search_phrase
+
+    # retrieve the search results for the current search
+    @search_results = find_current_search_results(@current_search_metadata)
+
+    # also pass id values for the current search, and platform
+    @search_id = @current_search.search_id
+    @platform_id = @current_search.platform_id
   end
+
 
   # POST /researcher_rankings
   # POST /researcher_rankings.json
   def create
     @researcher_ranking = ResearcherRanking.new(researcher_ranking_params)
-    @researcher_id = researcher_ranking_params["researcher_id"]
+    
+    # retrieve id values from the autofilled form params
+    @researcher_email = researcher_ranking_params["researcher_email"]
     @search_id = researcher_ranking_params["search_id"]
+    @platform_id = researcher_ranking_params["platform_id"]
+    @search_number = researcher_ranking_params["search_number"]
 
     respond_to do |format|
       if @researcher_ranking.save
         # on successful submission of form, check to see if the
         # researcher has responded to all queries in their
         # search_group_id. If so, send the user to a congratulations page
-        if survey_completed(@researcher_id, @search_id)
+        # nb: add 1 to the current search number because we start counting
+        # searches from 0, while a 1-question array has length 1
+        if survey_completed(@researcher_email, @search_number)
           format.html { redirect_to :controller => 'survey_completed', :action => 'show' }
+        
         else
-          format.html { redirect_to new_researcher_ranking_path(:researcher_id => @researcher_id, :search_id => find_next_search_result(@researcher_id, @search_id)), action: show, notice: 'Success! Your information has been saved.', status: 301 }
+          format.html { 
+            redirect_to new_researcher_ranking_path(
+              :researcher_email => @researcher_email,
+              :search_number => @search_number.to_i + 1
+            ),
+             action: show, 
+             notice: 'Success! Your information has been saved.', 
+             status: 301 }
+          
+
           format.json { render action: 'show', status: :created, location: @researcher_ranking }
         end
 
@@ -49,58 +81,84 @@ class ResearcherRankingsController < ApplicationController
     end
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_researcher_ranking
       @researcher_ranking = ResearcherRanking.find(params[:id])
     end
 
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def researcher_ranking_params
-      params.require(:researcher_ranking).permit(:search_id, :researcher_id, :result_1, :result_2, :result_3, :result_4, :result_5, :result_6, :result_7, :result_8, :result_9, :result_10)
+      params.require(:researcher_ranking).permit(:search_id, :platform_id, :researcher_email, :search_number, :result_1, :result_2, :result_3, :result_4, :result_5, :result_6, :result_7, :result_8, :result_9, :result_10)
     end
 
-    def retrieve_search_phrase(search_id_instance)
-      search = Search.where(search_id: search_id_instance)
-      
-      # nb: where queries always return an array, and
-      # each search object has a unique id, so call first
-      return search.first.search_phrase 
+
+    def find_current_search_metadata(researcher_email, search_number)
+      """
+      return a SearchGroup object that describes the current search
+      assigned to the researcher. This SearchGroup object will
+      have the search_id and platform_id properties required so that
+      we can uniquely identify the relevant Search object
+      """
+
+      researcher_search_group_id = Researcher.find_by(email: researcher_email).search_group_id
+      searches_assigned_to_researcher = SearchGroup.where(search_group_id: researcher_search_group_id)
+      return searches_assigned_to_researcher[search_number.to_i]
     end
 
-    def find_next_search_result(researcher_id_instance, search_id_instance)
-      researcher_search_group_id = Researcher.where(id: researcher_id_instance)[0].search_group_id
 
-      # retrieve all searches for the given search_group_id
-      relevant_searches = SearchGroup.where(search_group_id: researcher_search_group_id)
+    def find_current_search(current_search_metadata)
+      """
+      read in a SearchGroup object that contains keys for the 
+      search_id and platform_id that together describe the
+      search the current researcher should evaluate
+      """
 
-      # initialize a variable that represents the index
-      # position of the search we need to return
-      next_search_index = 0
+      # find the current search id and platform 
+      current_search_id = current_search_metadata.search_id
+      current_search_platform_id = current_search_metadata.platform_id
 
-      # find the index position of the current search id 
-      # within the relevant searches, and use this index
-      # to determine the next search to serve up. 
+      # find all searches with the current search id
+      candidate_searches = Search.where(search_id: current_search_id)
 
-      # NB: If the researcher has cycled through the 
-      # searches they need to answer, we should return
-      # a special flag that indicates the user is done
-      relevant_searches.each_with_index do |rs, rs_index|
-        if rs.search_id.to_i == search_id_instance.to_i
-          return relevant_searches[rs_index+1].search_id
-        end
-      end
-      return relevant_searches[next_search_index].search_id
+      # of those searches, return the one with the given platform id
+      return candidate_searches.where(platform_id: current_search_platform_id).take
     end
 
-    def survey_completed(researcher_id_instance, search_id_instance)
-      # to determine if researcher is done, retrieve their 
-      # search group id, then retrieve 
-      researcher_search_group_id = Researcher.find_by(id: researcher_id_instance).search_group_id
-      relevant_searches = SearchGroup.where(search_group_id: researcher_search_group_id)
-      last_relevant_search_id = relevant_searches.last.search_id
-      
-      if last_relevant_search_id.to_i == search_id_instance.to_i
+
+    def find_current_search_results(current_search_metadata)
+      """
+      read in a SearchGroup object that contains keys for the 
+      search_id and platform_id that together describe the
+      search for which this method will return the search results
+      """
+
+      # find the current search id and platform 
+      current_search_id = current_search_metadata.search_id
+      current_search_platform_id = current_search_metadata.platform_id
+
+      # return all search results for this search
+      candidate_search_results = SearchResult.where(search_id: current_search_id)
+
+      return candidate_search_results.where(platform_id: current_search_platform_id)
+    end
+
+
+    def survey_completed(researcher_email, search_number)
+      """
+      Read in the current researcher's email and the search number they 
+      just completed, and return true if they're done with the survey and 
+      false if they have more questions to answer
+      """ 
+      # nb: add 1 to the current search value to determine the number
+      # of completed searches, as we start the researcher on search number
+      # 0, while a single member list has length 1
+      completed_searches = search_number.to_i + 1
+      researcher_search_group_id = Researcher.find_by(email: researcher_email).search_group_id
+      searches_assigned_to_researcher = SearchGroup.where(search_group_id: researcher_search_group_id)
+      if completed_searches == searches_assigned_to_researcher
         return true
       else
         return false
