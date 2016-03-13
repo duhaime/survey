d5a1ffec408ab18300b3f304120959e90aaaeeb0
duhaime <douglas.duhaime@gmail.com>
2016-03-12 22:46:47 -0500

    add extension to db/schema.rb to support postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160312204747) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "platforms", force: true do |t|
     t.text     "platform_name"
     t.integer  "platform_id"
