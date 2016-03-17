a55ac15d55c6a128be77f1be2a5973a18d2f6bac
duhaime <douglas.duhaime@gmail.com>
2016-03-16 21:51:12 -0400

    update db schema to include search_phrase field in SearchGroup table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312204747) do
+ActiveRecord::Schema.define(version: 20160316115659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20160312204747) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "platform_id"
+    t.text     "search_phrase"
   end
 
   create_table "search_results", force: true do |t|
